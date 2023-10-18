270d8e0e388627a77bcc269d3583a04a1c22fa4c
Yennefer <1223696764@qq.com>
2023-10-18 23:20:20 +0800

    lulu 代码块的运行

--- src-tauri/src/runner.rs
+++ src-tauri/src/runner.rs
@@ -1,28 +1,33 @@
 use std::process::Command;
-
+use std::time::Instant;
 use tauri::command;
 
 #[derive(Debug, Clone, serde::Serialize)]
 pub struct Result {
     status: String,
     message: String,
+    time: f64,
 }
 
 #[command]
 pub fn run_code(code: String) -> Result {
+    let start = Instant::now();
     let output = Command::new("node").arg("-e").arg(code).output().unwrap();
+    let duration = start.elapsed().as_secs_f64();
 
     if output.status.success() {
         let stdout = String::from_utf8_lossy(&output.stdout);
         Result {
             status: "success".to_string(),
             message: stdout.to_string(),
+            time: format!("{:.1}", duration).parse().unwrap(),
         }
     } else {
         let stderr = String::from_utf8_lossy(&output.stderr);
         Result {
             status: "error".to_string(),
             message: stderr.to_string(),
+            time: format!("{:.1}", duration).parse().unwrap(),
         }
     }
 }
