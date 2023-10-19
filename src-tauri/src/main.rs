c1d888bed2bc6e4f6800c16610065dc263bb1aa4
Yennefer <1223696764@qq.com>
2023-10-20 00:39:05 +0800

    实现新建文件夹 修复新建时路径错误

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,51 +4,14 @@
 )]
 
 use file::{
-    create_file, create_folder, create_folder_all, delete_file, open_folder, read_file,
-    rename_file, write_file,
+    create_file, create_folder, delete_file, open_folder, read_file, rename_file, write_file,
 };
 mod file;
 
 use runner::run_code;
 mod runner;
 
-use kanban::{create_kanban, delete_kanban, get_kanban_list, read_kanban, save_kanban};
-mod kanban;
-
-use tauri::{
-    api::path::{self},
-    Config,
-};
-
-fn init() {
-    if let Some(mut p) = path::app_data_dir(&Config::default()) {
-        p.push("com.lulu");
-        if !p.exists() {
-            create_folder_all((&p.to_string_lossy()).to_string());
-        }
-
-        let config_path = p.join("config.json");
-        if !config_path.exists() {
-            create_file((&config_path.to_string_lossy()).to_string());
-            write_file(
-                (&config_path.to_string_lossy()).to_string(),
-                "{}".to_string(),
-            );
-        }
-
-        let kanban_path = p.join("kanban.json");
-        if !kanban_path.exists() {
-            create_file((&kanban_path.to_string_lossy()).to_string());
-            write_file(
-                (&kanban_path.to_string_lossy()).to_string(),
-                "{\"kanban\": []}".to_string(),
-            );
-        }
-    }
-}
-
 fn main() {
-    init();
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             open_folder,
@@ -58,12 +21,7 @@ fn main() {
             create_file,
             create_folder,
             delete_file,
-            rename_file,
-            read_kanban,
-            save_kanban,
-            get_kanban_list,
-            create_kanban,
-            delete_kanban
+            rename_file
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
