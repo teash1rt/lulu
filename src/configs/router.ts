6dfa5d80f6ade1d4c4576b1637362f32aa22190c
Yennefer <1223696764@qq.com>
2023-10-21 02:04:02 +0800

    kanban 基本实现

--- src/configs/router.ts
+++ src/configs/router.ts
@@ -1,7 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import pinia from '../stores/store'
-import { KanbanStore } from '../stores/KanbanStore'
-import { FileStore } from '../stores/FileStore'
 
 const routes = [
     {
@@ -18,32 +15,12 @@ const routes = [
         path: '/kanban',
         name: 'kanban',
         component: () => import('../views/KanbanView.vue')
-    },
-    {
-        path: '/settings',
-        name: 'settings',
-        component: () => import('../views/SettingsView.vue')
     }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
-})
-
-const kanbanStore = KanbanStore(pinia)
-const fileStore = FileStore(pinia)
-router.beforeEach(to => {
-    if (to.name === 'kanban') {
-        fileStore.lastSelect = null
-        fileStore.filePath = ''
-    } else if (to.name === 'file') {
-        kanbanStore.kanbanId = ''
-    } else {
-        fileStore.lastSelect = null
-        fileStore.filePath = ''
-        kanbanStore.kanbanId = ''
-    }
 })
 
 export default router
