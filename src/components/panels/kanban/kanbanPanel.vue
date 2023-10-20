8d8ea21eb59e46af7416e9822b861fbb93ae6c3f
Yennefer <1223696764@qq.com>
2023-10-21 02:04:02 +0800

    kanban 基本实现

--- src/components/panels/kanban/kanbanPanel.vue
+++ src/components/panels/kanban/kanbanPanel.vue
@@ -1,34 +1,39 @@
 <template>
     <div class="kanban-panel">
-        <div class="common-button" @click="handleAdd">
-            <svg-icon name="plus" color="#ffffff" class="icon" />
-            添加看板
+        <div class="add-kanban" @click="handleAdd">
+            <svg-icon name="plus" color="#ccc" class="icon" />
         </div>
-        <kanbanList />
     </div>
 </template>
 
 <script setup lang="ts">
-import kanbanList from './kanbanList.vue'
-import { WebviewWindow } from '@tauri-apps/api/window'
+import { useRouter } from 'vue-router'
 
-const handleAdd = async () => {
-    new WebviewWindow('newKanban', {
-        url: '/$/new-kanban',
-        decorations: false,
-        center: true,
-        width: 500,
-        height: 170,
-        resizable: false,
-        alwaysOnTop: true
-    })
+const router = useRouter()
+
+const handleAdd = () => {
+    router.push({ name: 'kanban' })
 }
 </script>
 
 <style lang="less" scoped>
+.kanban-panel {
+    .add-kanban {
+        color: #ffffff;
+        background-color: #e86a3d;
+        border-radius: 3px;
+        cursor: pointer;
+        width: 60%;
+        height: 45px;
+        margin: 0 auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+}
+
 .icon {
-    width: 23px;
-    height: 23px;
-    margin-right: 5px;
+    width: 25px;
+    height: 25px;
 }
 </style>
