f9bb14b3b3f2a0ef375c55042c022d6651801671
Yennefer <1223696764@qq.com>
2023-10-22 00:37:49 +0800

    kanban 数据文件初始化

--- src-tauri/src/file.rs
+++ src-tauri/src/file.rs
@@ -1,4 +1,5 @@
-use std::fs::{create_dir, read_dir, remove_file, rename, File, OpenOptions};
+use serde::Serialize;
+use std::fs::{create_dir, create_dir_all, read_dir, remove_file, rename, File, OpenOptions};
 use std::io::{Read, Write};
 use std::path::Path;
 use tauri::command;
@@ -6,7 +7,7 @@ use uuid::Uuid;
 
 pub static LEGAL_EXTENSIONS: [&str; 2] = ["md", "lulu"];
 
-#[derive(Debug, Clone, serde::Serialize)]
+#[derive(Debug, Serialize)]
 
 // file or folder
 pub struct Fof {
@@ -25,6 +26,7 @@ fn read_folder(path: &Path, mut level: u32) -> Vec<Fof> {
     level += 1;
     let paths = read_dir(path).unwrap();
     let mut folder_arr: Vec<Fof> = Vec::new();
+
     for p in paths {
         let uuid = Uuid::new_v4();
         let id = uuid.to_string().replace("-", "");
@@ -102,6 +104,10 @@ pub fn create_file(path: String) {
 #[command]
 pub fn create_folder(path: String) {
     create_dir(&path).unwrap();
+}
+
+pub fn create_folder_all(path: String) {
+    create_dir_all(&path).unwrap();
 }
 
 #[command]
