6d94861d624a76110a6d13cf1497ad1371eed8b2
Yennefer <1223696764@qq.com>
2023-10-22 00:37:49 +0800

    kanban 数据文件初始化

--- src/components/panels/kanban/kanbanPanel.vue
+++ src/components/panels/kanban/kanbanPanel.vue
@@ -2,17 +2,22 @@
     <div class="kanban-panel">
         <div class="add-kanban" @click="handleAdd">
             <svg-icon name="plus" color="#ccc" class="icon" />
+            添加看板
         </div>
+        <kanbanList />
     </div>
 </template>
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
+import kanbanList from './kanbanList.vue'
+import { invoke } from '@tauri-apps/api/tauri'
 
 const router = useRouter()
 
-const handleAdd = () => {
+const handleAdd = async () => {
     router.push({ name: 'kanban' })
+    await invoke('read_kanban_list')
 }
 </script>
 
@@ -20,11 +25,11 @@ const handleAdd = () => {
 .kanban-panel {
     .add-kanban {
         color: #ffffff;
-        background-color: #e86a3d;
+        background-color: #850bff;
         border-radius: 3px;
         cursor: pointer;
         width: 60%;
-        height: 45px;
+        height: 35px;
         margin: 0 auto;
         display: flex;
         justify-content: center;
