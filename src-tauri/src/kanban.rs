4cad4d17bbb4accf970cde54d402facea79113bb
Yennefer <1223696764@qq.com>
2023-10-22 00:37:49 +0800

    kanban 数据文件初始化

--- src-tauri/src/kanban.rs
+++ src-tauri/src/kanban.rs
@@ -1,23 +1,22 @@
-use crate::file::{read_file, write_file};
+use crate::file::read_file;
 use serde::{Deserialize, Serialize};
-use serde_json::{from_str, to_string};
+use serde_json::from_str;
 use tauri::command;
 use tauri::{
     api::path::{self},
     Config,
 };
-use uuid::Uuid;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct KanbanColumn {
+#[derive(Debug, Serialize, Deserialize)]
+struct KanbanColumn {
     column_type: String,
-    content: Vec<String>,
+    content: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct KanbanInfo {
     id: String,
-    name: String,
+
     info: Vec<KanbanColumn>,
 }
 
@@ -26,104 +25,19 @@ struct Kanban {
     kanban: Vec<KanbanInfo>,
 }
 
-#[derive(Debug, Serialize)]
-pub struct KanbanListItem {
-    id: String,
-    name: String,
-}
-
-#[derive(Debug, Serialize)]
-struct KanbanDetails {
-    path: String,
-    data: Kanban,
-}
-
-fn get_kanban_content() -> KanbanDetails {
-    let path = path::app_data_dir(&Config::default())
+#[command]
+pub fn read_kanban_list() {
+    let p = path::app_data_dir(&Config::default())
         .unwrap()
         .join("com.lulu")
         .join("kanban.json")
         .to_string_lossy()
         .to_string();
-    let content = read_file(path.clone());
+    let content = read_file(p);
     let data: Kanban = from_str(&content).unwrap();
-    KanbanDetails { path, data }
-}
 
-#[command]
-pub fn get_kanban_list() -> Vec<KanbanListItem> {
-    let data = get_kanban_content().data;
-    let mut kanban_list: Vec<KanbanListItem> = Vec::new();
-
+    // 输出id属性的值
     for kanban in data.kanban {
-        kanban_list.push(KanbanListItem {
-            id: kanban.id,
-            name: kanban.name,
-        });
+        println!("id: {}", kanban.id);
     }
-    kanban_list
-}
-
-#[command]
-pub fn read_kanban(id: String) -> Vec<KanbanColumn> {
-    let data = get_kanban_content().data;
-    let mut column: Vec<KanbanColumn> = Vec::new();
-    for kanban in data.kanban {
-        if kanban.id == id {
-            column = kanban.info
-        }
-    }
-    column
-}
-
-#[command]
-pub fn create_kanban(name: String) {
-    let details = get_kanban_content();
-    let mut data = details.data;
-    let id = Uuid::new_v4().to_string();
-    data.kanban.push(KanbanInfo {
-        id: id[0..8].to_string(),
-        name,
-        info: [
-            KanbanColumn {
-                column_type: "To do".to_string(),
-                content: [].to_vec(),
-            },
-            KanbanColumn {
-                column_type: "In progress".to_string(),
-                content: [].to_vec(),
-            },
-            KanbanColumn {
-                column_type: "Completed".to_string(),
-                content: [].to_vec(),
-            },
-        ]
-        .to_vec(),
-    });
-    let json_data = to_string(&data).unwrap();
-    write_file(details.path, json_data);
-}
-
-#[command]
-pub fn delete_kanban(id: String) {
-    let details = get_kanban_content();
-    let mut data = details.data;
-    data.kanban.retain(|kanban| kanban.id != id);
-    let json_data = to_string(&data).unwrap();
-    write_file(details.path, json_data);
-}
-
-#[command]
-pub fn save_kanban(id: String, columns: Vec<KanbanColumn>) {
-    let details = get_kanban_content();
-    let mut data = details.data;
-    for kanban in &mut data.kanban {
-        if kanban.id == id {
-            let kanban_column: Vec<KanbanColumn> = from_str(&to_string(&columns).unwrap()).unwrap();
-            kanban.info = kanban_column;
-            break;
-        }
-    }
-    let json_data = to_string(&data).unwrap();
-    write_file(details.path, json_data);
 }
