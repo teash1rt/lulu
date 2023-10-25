674ffee261347e7ce088a8874fbedfa9b78af067
Yennefer <1223696764@qq.com>
2023-10-26 00:20:31 +0800

    实现关闭时先保存文件

--- src/types/BusEvent.ts
+++ src/types/BusEvent.ts
@@ -1,13 +1,6 @@
-/**
- * @meaning 事件枚举
- */
-
 export enum BusEvent {
     SwitchFilePath = 'SwitchFilePath',
     SaveFile = 'SaveFile',
     GetToc = 'GetToc',
-    SaveCompleted = 'SaveCompleted',
-    CreateFile = 'CreateFile',
-    CreateFolder = 'CreateFolder',
-    CreateKanban = 'CreateKanban'
+    SaveCompleted = 'SaveCompleted'
 }
