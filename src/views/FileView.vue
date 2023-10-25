6f987f82ac18384f213e822b3ed33726f2fc8a54
Yennefer <1223696764@qq.com>
2023-10-26 00:20:31 +0800

    实现关闭时先保存文件

--- src/views/FileView.vue
+++ src/views/FileView.vue
@@ -50,7 +50,7 @@ const fileRef = ref<InstanceType<typeof mdEditor | typeof luluEditor> | null>(nu
 
 listen(BusEvent.SaveFile, async () => {
     await fileRef.value!.saveFile()
-    await emit(BusEvent.SaveCompleted)
+    emit(BusEvent.SaveCompleted)
 })
 </script>
 
