97ae0c22ba77ee59a1821bddd3429ec662881ce7
Yennefer <1223696764@qq.com>
2023-10-27 18:24:14 +0800

    补充类型定义的注释

--- src/types/Kanban.ts
+++ src/types/Kanban.ts
@@ -1,12 +1,24 @@
+/**
+ * @meaning kanban 的原始列数据
+ */
+
 export type KanbanColumn = {
     column_type: string
     content: string[]
 }
 
+/**
+ * @meaning 已创建的 kanban 列表
+ */
+
 export type KanbanListItem = {
     id: string
     name: string
 }
+
+/**
+ * @meaning 封装了uuid 后的 kanban 列数据
+ */
 
 export type KanbanColumnInfo = {
     column_type: string
