db7ed5edd2a7e6d131876193508a9950150da2f3
Yennefer <1223696764@qq.com>
2023-10-28 16:59:00 +0800

    将弹窗代码从 public 移动到 vue 中

--- src/configs/router.ts
+++ src/configs/router.ts
@@ -7,22 +7,58 @@ const routes = [
     {
         path: '/',
         name: 'home',
-        component: () => import('../views/HomeView.vue')
+        component: () => import('../views/HomeView.vue'),
+        meta: {
+            type: 'view'
+        }
     },
     {
         path: '/file',
         name: 'file',
-        component: () => import('../views/FileView.vue')
+        component: () => import('../views/FileView.vue'),
+        meta: {
+            type: 'view'
+        }
     },
     {
         path: '/kanban',
         name: 'kanban',
-        component: () => import('../views/KanbanView.vue')
+        component: () => import('../views/KanbanView.vue'),
+        meta: {
+            type: 'view'
+        }
     },
     {
         path: '/settings',
         name: 'settings',
-        component: () => import('../views/SettingsView.vue')
+        component: () => import('../views/SettingsView.vue'),
+        meta: {
+            type: 'view'
+        }
+    },
+    {
+        path: '/$/new-file',
+        name: 'newFile',
+        component: () => import('../views/windows/NewFile.vue'),
+        meta: {
+            type: 'window'
+        }
+    },
+    {
+        path: '/$/new-folder',
+        name: 'newFolder',
+        component: () => import('../views/windows/NewFolder.vue'),
+        meta: {
+            type: 'window'
+        }
+    },
+    {
+        path: '/$/new-kanban',
+        name: 'newKanban',
+        component: () => import('../views/windows/NewKanban.vue'),
+        meta: {
+            type: 'window'
+        }
     }
 ]
 
